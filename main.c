12566f397bddf7a01dbcb29ba94474098e5ee924
BVinc06 <BVinc06@github.com>
2017-10-27 10:07:43 +0200

    addition

---
+++ main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Addition
+//
+//  Created by Berthaud Vincent on 27/10/2017.
+//  Copyright © 2017 Berthaud Vincent. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int addition (int a, int b)
+{
+    return a+b;
+}
+
+int main()
+{
+    printf("%d\n", addition(1,4));
+    return 0;
+}
