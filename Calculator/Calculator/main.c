e31a601b5c47f55bb95fa024008f1f8e50909587
BVinc06 <BVinc06@github.com>
2017-10-27 09:40:26 +0200

    Add main program

---
+++ Calculator/Calculator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Calculator
+//
+//  Created by Berthaud Vincent on 27/10/2017.
+//  Copyright © 2017 Berthaud Vincent. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
